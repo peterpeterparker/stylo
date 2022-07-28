eec306880eab6f56496dbae2aadc6e6edabdc72a
peterpeterparker <david.dalbusco@outlook.com>
2022-07-28 16:32:47 +0200

    feat: create sublist improved

--- src/events/tab.events.ts
+++ src/events/tab.events.ts
@@ -1,7 +1,7 @@
 import {moveCursorToEnd} from '@deckdeckgo/utils';
 import containerStore from '../stores/container.store';
 import {isTextNode} from '../utils/node.utils';
-import {findParagraph, isParagraphList} from '../utils/paragraph.utils';
+import {findParagraph, isParagraph, isParagraphList} from '../utils/paragraph.utils';
 import {getRange} from '../utils/selection.utils';
 
 export class TabEvents {
@@ -39,10 +39,8 @@ export class TabEvents {
       container: containerStore.state.ref
     });
 
-    const {listCreated} = await this.createSublist({paragraph, node, range, shiftKey});
-
-    if (listCreated) {
-      $event.preventDefault();
+    if (paragraph && isParagraphList({paragraph})) {
+      await this.createSublist({$event, paragraph, node, range, shiftKey});
       return;
     }
 
@@ -51,21 +49,35 @@ export class TabEvents {
       return;
     }
 
+    this.createTabulation({range, $event, node, paragraph});
+  }
+
+  private createTabulation({
+    range,
+    $event,
+    node,
+    paragraph
+  }: {
+    range: Range;
+    $event: KeyboardEvent;
+    node: Node | undefined;
+    paragraph: Node | undefined;
+  }) {
     $event.preventDefault();
 
     if (!isTextNode(node)) {
       if (paragraph !== undefined) {
-        this.createTabulation({range});
+        this.insertSpanTabulation({range});
         return;
       }
 
       return;
     }
 
-    this.createTabulation({range});
+    this.insertSpanTabulation({range});
   }
 
-  private createTabulation({range}: {range: Range}) {
+  private insertSpanTabulation({range}: {range: Range}) {
     const span: HTMLSpanElement = document.createElement('span');
     span.innerHTML = '\u0009';
 
@@ -77,57 +89,60 @@ export class TabEvents {
   private async createSublist({
     paragraph,
     node,
-    range: {endOffset, commonAncestorContainer},
-    shiftKey
+    range,
+    shiftKey,
+    $event
   }: {
-    paragraph: Node | undefined;
+    $event: KeyboardEvent;
+    paragraph: Node;
     node: Node | undefined;
     range: Range;
     shiftKey: boolean;
-  }): Promise<{listCreated: boolean}> {
-    if (!paragraph) {
-      return {listCreated: false};
-    }
+  }): Promise<void> {
+    // If list contains a single child that is just text then browser returns the list as focus node
+    const focusNodeIsList: boolean = node !== undefined && isParagraphList({paragraph: node});
 
-    if (!isParagraphList({paragraph})) {
-      return {listCreated: false};
-    }
-
-    const li: Node | undefined = this.findParentElement({
-      element: node,
-      paragraph,
-      nodeName: 'li'
-    });
+    const li: Node | undefined = focusNodeIsList
+      ? node.firstChild
+      : this.findParentElement({
+          element: node,
+          paragraph,
+          nodeName: 'li'
+        });
 
     if (!li) {
-      return {listCreated: false};
+      this.createTabulation({range, $event, node, paragraph});
+      return;
+    }
+
+    const ul: Node | undefined = focusNodeIsList
+      ? node
+      : this.findParentElement({
+          element: node,
+          paragraph,
+          nodeName: 'ul'
+        });
+
+    if (!ul) {
+      this.createTabulation({range, $event, node, paragraph});
+      return;
     }
 
     if (shiftKey) {
-      const cursorStart: boolean = endOffset === 0;
-      const firstChild: boolean = commonAncestorContainer.isSameNode(li.firstChild);
-
-      if (!cursorStart || !firstChild) {
-        return {listCreated: false};
-      }
-
-      const ul: Node | undefined = this.findParentElement({
-        element: node,
-        paragraph,
-        nodeName: 'ul'
-      });
-
-      if (!ul) {
-        return {listCreated: false};
+      // We do not want to remove the top ul
+      if (isParagraph({element: ul, container: containerStore.state.ref})) {
+        return;
       }
 
       // Li is the sole child element, we can replace the ul
       if (ul.childNodes.length === 1) {
-        // Extract li
-        const range: Range = new Range();
-        range.selectNode(li);
+        $event.preventDefault();
 
-        const contents: DocumentFragment = range.cloneContents();
+        // Extract li
+        const newRange: Range = new Range();
+        newRange.selectNode(li);
+
+        const contents: DocumentFragment = newRange.cloneContents();
 
         ul.parentElement.replaceChild(contents, ul);
         return;
@@ -143,47 +158,74 @@ export class TabEvents {
       return;
     }
 
+    const {endOffset, commonAncestorContainer} = range;
+
     const cursorEnd: boolean = endOffset === commonAncestorContainer.textContent.length;
-    const lastChild: boolean = commonAncestorContainer.isSameNode(li.lastChild);
+    const lastChild: boolean = commonAncestorContainer.isSameNode(this.findLastChild(li));
 
     if (!cursorEnd || !lastChild) {
-      return {listCreated: false};
+      this.createTabulation({range, $event, node, paragraph});
+      return;
     }
 
-    // Clone li and append it to a new sublist, a new ul
-    const range: Range = new Range();
-    range.selectNode(li);
+    // We do not want to index list that has a single element
+    if (ul.childNodes.length === 1) {
+      this.createTabulation({range, $event, node, paragraph});
+      return;
+    }
 
-    const contents: DocumentFragment = range.cloneContents();
-    const ul: HTMLUListElement = document.createElement('ul');
-    ul.append(contents);
+    $event.preventDefault();
+
+    // Clone li and append it to a new sublist, a new ul
+    const newRange: Range = new Range();
+    newRange.selectNode(li);
+
+    const contents: DocumentFragment = newRange.cloneContents();
+    const newUl: HTMLUListElement = document.createElement('ul');
+    newUl.append(contents);
 
     // Move cursor end to newly created list
     const observer: MutationObserver = new MutationObserver(() => {
       observer.disconnect();
 
-      moveCursorToEnd(ul);
+      // Move cursor to new li
+      moveCursorToEnd(newUl.firstChild);
     });
 
     observer.observe(paragraph, {childList: true, subtree: true});
 
     // Replace li with new ul
-    li.parentElement.replaceChild(ul, li);
-
-    return {listCreated: true};
+    li.parentElement.replaceChild(newUl, li);
   }
 
-  private findParentElement({element, paragraph, nodeName}: {element: Node; paragraph: Node; nodeName: string}): Node | undefined {
-    if (element.isEqualNode(paragraph)) {
-      return undefined;
-    }
-
+  private findParentElement({
+    element,
+    paragraph,
+    nodeName
+  }: {
+    element: Node;
+    paragraph: Node;
+    nodeName: string;
+  }): Node | undefined {
     const {nodeName: elementNodeName, parentElement} = element;
 
-    if (elementNodeName.toLowerCase() === 'li') {
+    if (elementNodeName.toLowerCase() === nodeName) {
       return element;
     }
 
+    // We iterated till the paragraph and even the paragraph is not the nodeName we are looking for
+    if (isParagraph({element, container: containerStore.state.ref})) {
+      return undefined;
+    }
+
     return this.findParentElement({element: parentElement, paragraph, nodeName});
+  }
+
+  private findLastChild(element: Node): Node {
+    if (element.lastChild !== null) {
+      return this.findLastChild(element.lastChild);
+    }
+
+    return element;
   }
 }
