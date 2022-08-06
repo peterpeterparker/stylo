05785e1e4d8595ee47171a094a2e3b21085e5b95
slothicp <slothicp@gmail.com>
2022-08-07 04:23:39 +0900

    Add ja.ts file

---
+++ src/assets/i18n/ja.ts
@@ -0,0 +1,46 @@
+import {I18n} from '../../types/i18n';
+
+export const ja: I18n = {
+  lang: 'ja',
+  plugins: {
+    huge_title: '大見出し',
+    large_title: '中見出し',
+    small_title: '小見出し',
+    paragraph: '段落',
+    separator: '区切り文字',
+    unordered_list: '箇条書きリスト',
+    ordered_list: '番号付きリスト',
+    image: '画像',
+    code: 'コード',
+    no_matches: '該当なし'
+  },
+  add: {
+    placeholder: '「/」を押してプラグインを表示',
+    add_element: '新しいパーツを追加'
+  },
+  toolbar: {
+    align_left: '左寄せ',
+    align_center: '中央揃え',
+    align_right: '右寄せ',
+    font_size: '文字サイズ',
+    list_ol: '番号付きリスト',
+    list_ul: '箇条書きリスト',
+    style_list: 'スタイルの一覧',
+    style_align: 'スタイルの整列',
+    style_font_size: 'スタイルの文字サイズ',
+    style_color: 'スタイルの色',
+    style_background: 'スタイルの背景',
+    link: 'リンクを作成または削除',
+    bold: '太字',
+    italic: '斜体',
+    underline: '下線',
+    strikethrough: '打ち消し線'
+  },
+  menus: {
+    img_width_original: '元の画像幅',
+    img_width_large: '大きい画像幅',
+    img_width_medium: '中くらいの画像幅',
+    img_width_small: '小さい画像幅',
+    img_delete: '画像を削除'
+  }
+};
