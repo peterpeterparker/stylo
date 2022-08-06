14dff35430bb44d25f87006ee8e95d5cb8f59f3b
peterpeterparker <david.dalbusco@outlook.com>
2022-08-06 21:41:44 +0200

    feat: load japanese

--- src/stores/i18n.store.ts
+++ src/stores/i18n.store.ts
@@ -23,6 +23,11 @@ const nlI18n = async (): Promise<I18n> => {
   return nl;
 };
 
+const jaI18n = async (): Promise<I18n> => {
+  const {ja} = await import(`../assets/i18n/ja`);
+  return ja;
+};
+
 const enI18n = (): I18n => en;
 
 onChange('lang', async (lang: Languages) => {
@@ -37,6 +42,9 @@ onChange('lang', async (lang: Languages) => {
       break;
     case 'nl':
       bundle = await nlI18n();
+      break;
+    case 'ja':
+      bundle = await jaI18n();
       break;
     default:
       bundle = enI18n();
