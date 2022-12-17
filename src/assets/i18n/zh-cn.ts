fafa08727c4fa90e542f827225dab72b1d834698
Sun <vansdaynet@gmail.com>
2022-12-17 07:32:23 -0800

    Create zh-cn.ts
    add Simple Chinese translation

---
+++ src/assets/i18n/zh-cn.ts
@@ -0,0 +1,47 @@
+import {I18n} from '../../types/i18n';
+
+export const en: I18n = {
+  lang: 'zh-cn',
+  plugins: {
+    huge_title: '特大标题',
+    large_title: '大标题',
+    small_title: '小标题',
+    paragraph: '段落',
+    separator: '分割符',
+    unordered_list: '符号列表',
+    ordered_list: '数字列表',
+    image: '图片',
+    code: '代码',
+    no_matches: '无匹配',
+    blockquote: '块级引用'
+  },
+  add: {
+    placeholder: '按 "/" 增加插件',
+    add_element: '增加一个新部分'
+  },
+  toolbar: {
+    align_left: '左对齐',
+    align_center: '居中',
+    align_right: '右对齐',
+    font_size: '字体大小',
+    list_ol: '排序列表',
+    list_ul: '无需列表',
+    style_list: '风格列表',
+    style_align: '风格对准',
+    style_font_size: '风格字体大小',
+    style_color: '风格颜色',
+    style_background: '风格背景',
+    link: '创建或删除链接',
+    bold: '粗体',
+    italic: '斜体',
+    underline: '下划线',
+    strikethrough: '删除线'
+  },
+  menus: {
+    img_width_original: '原始图片宽度',
+    img_width_large: '大图片宽度',
+    img_width_medium: '中图片宽度',
+    img_width_small: '小图片宽度',
+    img_delete: '删除图片'
+  }
+};
