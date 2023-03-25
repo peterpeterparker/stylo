d35e0dfa91cb0ac01d6460cfe800c7ed64462182
David Dal Busco <david.dalbusco@outlook.com>
2023-03-25 08:49:57 +0100

    test: adapt icons

--- src/components/plugins/add/add.spec.ts
+++ src/components/plugins/add/add.spec.ts
@@ -12,8 +12,9 @@ describe('add', () => {
       <stylo-add style="display: none;">
        <mock:shadow-root>
          <button aria-label="Add a new part" type="button">
-            <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-               <path d="M256 112v288M400 256H112" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></path>
+            <svg fill="currentColor" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+               <path d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
             </svg>
           </button>
        </mock:shadow-root>
